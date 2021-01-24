9a8dbed112ef1ca41fd0321993fd66e139dc3717
Olivia Lewis <ollybaby12@gmail.com>
2021-01-24 17:22:37 -0500

    update App.js to render Tab.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Tooltip from './composition/Tooltip';
 import TheDate from './state/TheDate';
 import Counter from './state/Counter';
+import Tabs from './state/Tabs'
 
 //make 2 tooltips here and another inside the App directly
 const firstTooltip = (
@@ -17,27 +18,21 @@ const secondTooltip= (
     officiis
   </Tooltip>
 )
+const tabsProp = [
+  {name: 'First tab',
+   content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.'},
+  {name: 'Second tab',
+   content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+  {name: 'Third tab',
+    contnet:'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.'},
+];
 
-function App() {
-  return (
-    <main className='App'>
-      <TheDate />
-  
-      <Counter count={123} />
-
-      <Split className='left' flexBasis='2'>
-      This is the content change for the left `Split`. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
-      {/* make another tooltip directly inside the App */}
-      <Tooltip message='one more tooltip message'>
-        Necessitatibus?
-      </Tooltip>
-      </Split>
-      <Split className='right'>
-      This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-
-    </main>
-  );
+class App extends React.Component {
+  render(){
+   return <div>
+     <Tabs tabs={tabsProp} />
+     </div>
+  }
 }
 
 export default App;
