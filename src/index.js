cea59bb37b9e1d9bc9ee64780c0e2f23e45e3a50
Olivia Lewis <ollybaby12@gmail.com>
2021-02-19 21:14:18 -0500

    lang-context files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 // import App from './App';
-import App from './paltrows-power-toes/App'
+// import App from './paltrows-power-toes/App'
+import AppLang from './lang-context/AppLang';
 import './index.css';
 // import HelloWorld from './state-drills/HelloWorld';
 // import Bomb from './state-drills/Bomb';
@@ -10,9 +11,7 @@ import './index.css';
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <AppLang />, 
     document.getElementById('root')
     );
 //ReactDOM.render(<HelloWorld />, document.getElementById('root'));
