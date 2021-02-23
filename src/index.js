d6ecbc8fc4271c865aa28fce4cee4f252c370a8d
Olivia Lewis <ollybaby12@gmail.com>
2021-02-23 18:15:27 -0500

    implement registrationForm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 // import App from './App';
 // import App from './paltrows-power-toes/App'
-import AppLang from './lang-context/AppLang';
+// import AppLang from './lang-context/AppLang';
+import RegistrationForm from './registrationForm/registrationForm';
 import './index.css';
 // import HelloWorld from './state-drills/HelloWorld';
 // import Bomb from './state-drills/Bomb';
@@ -11,7 +12,7 @@ import './index.css';
 
 
 ReactDOM.render(
-    <AppLang />, 
+    <RegistrationForm />, 
     document.getElementById('root')
     );
 //ReactDOM.render(<HelloWorld />, document.getElementById('root'));
